72ce12ed410c1c2f6b2bb01d241680e5eabb7472
bianca-limachi <bianca.limachi@ucb.edu.bo>
2025-04-10 09:48:18 -0400

    Add files via upload

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,35 @@
+#CREAR UNA VAR
+saludo= "Hola mundo!!"
+print (saludo)
+
+#Suma de numeros
+num1=10
+num2=20
+suma= num1+num2
+print(suma)
+
+#suma de palabras -> conectar palabras
+palabra1= "hola"
+palabra2= "mundo"
+suma_palabras = palabra1+ " " +palabra2
+print(suma_palabras)
+
+#resta
+var1=10
+var2=4
+resta=var1-var2
+print(resta)
+
+#multplicacion
+
+base=5
+altura=9
+area=base*altura
+print(area)
+
+#division
+
+var1=15
+var2=2
+dividir=var1/var2
+print(dividir)
